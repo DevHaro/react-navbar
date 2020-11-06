142444a88d818f1a883adefb9ca628d4c17287a9
DevHaro <harojose2019@gmail.com>
2020-11-05 18:59:43 -0600

    First commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+  {
+    title: 'Channel 1',
+    url: '#home',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Channel 2',
+    url: '#services',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Channel 3',
+    url: '#products',
+    cName: 'nav-links'
+  }
+]
